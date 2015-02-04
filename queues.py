1c2e1750fead84c8b3f3b21e3a348b5a0ec1d969
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-02-04 20:46:34 +1100

    Coursera Algorithms 1 week 2 assignment. First commit. Not working yet.

---
+++ queues.py
@@ -0,0 +1,28 @@
+#Coursera Algorithms 1 - week 2
+
+class Deque():
+
+    def Deque(self):
+        self.deque = deque[]
+
+    def isEmpty(self, x):
+        return len(x) > 0
+
+    def size(self, x):
+        return len(x)
+
+    def addFirst(self, x, item):
+        x.appendleft(item)
+
+    def addLast(selfx, item):
+        x.appendRight(item)
+
+    def removeFirst(self, x):
+        x.popleft()
+
+    def removeLast(self, x):
+        x.pop()
+
+    def iterator(self, x):
+        for i in range(0, len(x)):
+            print i,
