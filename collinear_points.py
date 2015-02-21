e68a02c13ea758da23a50fd566c73d6444c4a53e
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-02-21 18:41:10 +1100

    Fixed problem in comparator. Now problem is in find min and max. P returned correctly.

--- collinear_points.py
+++ collinear_points.py
@@ -16,20 +16,25 @@ class Point(object):
             if q.x == self.x and q.y == self.y:  # if point is not self, find the slope
                 dy_dx = -999
             else:
-                dy_dx = (q.y - self.y) / (q.x - self.x)
+                dx = (q.x - self.x)
+                if dx == 0:
+                    dy_dx = "Inf"
+                else:
+                    dy_dx = (q.y - self.y) / dx
             slope.append(dy_dx)
         slope_key = sorted(range(len(slope)), key=lambda k: slope[k])
         slope.sort()
-
         i = 1
         while i < (len(slope) - 1):
-            collinear_points = [slope_key[0], slope_key[1]]
+            collinear_points = [slope_key[0], slope_key[i]]
             while slope[i] == slope[i + 1]:
                 collinear_points.append(slope_key[i + 1])
                 i += 1
+                if i == len(slope) - 1:
+                    break
             i += 1
-        if len(collinear_points) >= 4:
-            return collinear_points
+            if len(collinear_points) >= 4:
+                return collinear_points
         else:
             return None
 
@@ -56,34 +61,35 @@ def draw_to(min_point, max_point, point_list):
     for line in range(0, len(max_point)):
         x2.append([point_array[min_point[line][0]][0], point_array[max_point[line][0]][0]])
         y2.append([point_array[min_point[line][0]][1], point_array[max_point[line][0]][1]])
-        if len(x2) == 1:
-            x2 = x2[0]
-            y2 = y2[0]
+    if len(x2) == 1:
+        x2 = x2[0]
+        y2 = y2[0]
     plt.plot(x, y, 'ro', x2, y2)
     plt.axis([0, max(x)*1.2, 0, max(y)*1.2])
     plt.show()
 
 def find_first(point_list, key):
-    min = key[1]
+    print key
+    min = key[0]
     for i in range(1, len(key)):
-        if point_list[key[i]].x < point_list[key[min]].x:
+        if point_list[key[i]].x < point_list[min].x:
             min = key[i]
-        elif point_list[key[i]].x == point_list[key[min]].x:
-            if point_list[key[i]].y < point_list[key[min]].y:
+        elif point_list[key[i]].x == point_list[min].x:
+            if point_list[key[i]].y < point_list[min].y:
                 min = key[i]
     return min
 
 def find_last(point_list, key):
-    max = key[1]
+    max = key[0]
     for i in range(1, len(key)):
-        if point_list[key[i]].x > point_list[key[max]].x:
+        if point_list[key[i]].x > point_list[max].x:
             max = key[i]
-        elif point_list[key[i]].x == point_list[key[max]].x:
-            if point_list[key[i]].y > point_list[key[max]].y:
+        elif point_list[key[i]].x == point_list[max].x:
+            if point_list[key[i]].y > point_list[max].y:
                 max = key[i]
     return max
 
-with open('C:/Users/Lisa/Documents/code/collinear/input6.txt') as f:
+with open('C:/Users/Lisa/Documents/code/collinear/input8.txt') as f:
     next(f)
     point_array = [[float(digit) for digit in line.split()] for line in f]
 
@@ -100,23 +106,24 @@ p = []
 for point in point_collection:
     p.append(point.comparator(point_collection))
 
+# remove none from p
+p = [x for x in p if x is not None]
+
 if len(p) > 1:
     line = 1
     while line < len(p):
-        if p[line] is None:
-            p.pop(line)
-        elif p[line].sort() ==  p[line - 1].sort():
+        if sorted(p[line]) == sorted(p[line - 1]):
             p.pop(line)
         else:
-            line = line + 1
+            line += 1
 
 max_point = []
 min_point = []
 for i in p:
-    if i is not None:
-        min_point.append([find_first(point_collection, i)])
-        max_point.append([find_last(point_collection, i)])
-
+    min_point.append([find_first(point_collection, i)])
+    max_point.append([find_last(point_collection, i)])
+print max_point
+print min_point
 draw_to(min_point, max_point, point_array)
 
 class CollinearPoints(unittest.TestCase):
