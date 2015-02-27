4f533d4e06676dc2492b49b0415822cad156eb5b
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-02-27 14:04:32 +1100

    Added board initialization values.

--- 8-puzzle.py
+++ 8-puzzle.py
@@ -4,11 +4,14 @@ import math
 
 class Board(object):
 
-    def __init__(self, board):
+    def __init__(self, board, moves_to_reach, previous_node):
         self.board = board
         self.N = np.shape(self.board)[1]
+        self.moves_to_reach = moves_to_reach
+        self.previous_node = previous_node
 
     def hamming(self, moves):
+        '''number of blocks in wrong position + number of moves to get to node'''
         wrong_position = 0
         for i in range(0, self.N):
             for j in range(0, self.N):
@@ -17,6 +20,7 @@ class Board(object):
         return wrong_position + moves
 
     def manhattan(self, moves):
+        '''calculates total distance to end + number of moves to get to node'''
         total_distance = 0
         for i in range(0, self.N):
             for j in range(0, self.N):
@@ -36,11 +40,45 @@ class Board(object):
         else:
             return False
 
+    def find_empty(self):
+        for i in range(0, self.N):
+            for j in range(0, self.N):
+                if self.board[i][j] == 0:
+                    empty_i = i
+                    empty_j = j
+                    break
+        return [empty_i, empty_j]
+
+    def find_neighbours(self):
+        i = self.board.find_empty()[1]
+        j = self.board.find_empty()[2]
+        neighbours = []
+        if j > 0:
+            new = self.board
+            new[i][j] = new[i][j - 1]
+            new[i][j - 1] = 0
+            neighbours.append(new)
+        if j < N:
+            new = self.board
+            new[i][j] = new[i][j + 1]
+            new[i][j + 1] = 0
+            neighbours.append(new)
+        if i < N:
+            new = self.board
+            new[i][j] = new[i + 1][j]
+            new[i + 1][j] = 0
+            neighbours.append(new)
+        if i > 0:
+            new = self.board
+            new[i][j] = new[i - 1][j]
+            new[i - 1][j] = 0
+            neighbours.append(new)
 
 class Solver(object):
 
     def solver(self):
         pass
+
 
 with open('C:/Users/Lisa/Documents/code/8puzzle/puzzle25.txt') as f:
     next(f)
@@ -48,14 +86,14 @@ with open('C:/Users/Lisa/Documents/code/8puzzle/puzzle25.txt') as f:
 
 print board
 
-board = Board(board)
+board = Board(board, moves_to_reach=0, previous_node=None)
 
 
 class EightPuzzle(unittest.TestCase):
     with open('C:/Users/Lisa/Documents/code/8puzzle/puzzle25.txt') as f:
         next(f)
         board = [[float(digit) for digit in line.split()] for line in f]
-    board = Board(board)
+    board = Board(board, moves_to_reach=0, previous_node=None)
 
     def test_hamming(self):
         self.assertEqual(7, board.hamming(moves=0))
@@ -67,5 +105,5 @@ class EightPuzzle(unittest.TestCase):
         self.assertEqual(False, board.is_goal())
 
     def test_is_goal_on_true(self):
-        goal_board = Board([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
+        goal_board = Board([[1, 2, 3], [4, 5, 6], [7, 8, 0]], moves_to_reach=0, previous_node=None)
         self.assertTrue(goal_board.is_goal())
